6a3f85d30cbb0ff6415aed82e00008136db75f07
Ken Joshua Buenavides <kenjoshuabuenavides@gmail.com>
2024-09-30 19:52:08 +0700

    Delete and Update function

--- script.js
+++ script.js
@@ -1,29 +1,61 @@
  var todo = document.getElementById("todo");
  var description = document.getElementById("description");
  var date = document.getElementById("date");
+ var updateBtn = document.getElementById("update-btn");
+ var addBtn = document.getElementById("add-btn");
+ var errorText = document.getElementById('error-container');
+ var error = localStorage.setItem('error', '');
 
  var todoVal = "";
  var descriptionVal = "";
  var dateVal = ""
- var id = 0;
  var card = "";
- var arr_todo = [];
-
-//  var arr_todo = [];
 
 
-//  var arr_todo = [];
-//  var new_todo = {
-//   'title': 'Todo',
-//   'description' : 'description'
-//  }
-//  arr_todo.push(new_todo)
-//  console.log(arr_todo)
-// localStorage.setItem("todoList", JSON.stringify(arr_todo));
+ window.addEventListener('load', function(){
+  // sessionStorage.clear();
+  retrieve();
+  var editId;
 
-//  var a = localStorage.getItem('todoList');
-//  var b = JSON.parse(a)
-//  console.log(b[0].title)
+
+  //  Add Button Todo List
+  addBtn.addEventListener('click', () => {
+    if(todoVal !== "" && descriptionVal !== "" && dateVal !== ""){
+      store(todoVal, descriptionVal, dateVal);
+      retrieve();
+    } else {
+      errorText.innerHTML = `<p class="error-text">Please fill in all fields.</p>`;
+      
+    }
+      clear();
+  });
+
+  //  Delete Button Todo List
+  var deleteButtons = document.getElementsByName('delete-button');
+    deleteButtons.forEach(deleteBtn => {
+      deleteBtn.addEventListener('click', function() {
+        const attributeValue = this.getAttribute('data-id');
+        deleteTodoList(attributeValue);
+      });
+    });
+
+    // Edit Button Todo List
+    var editButtons = document.getElementsByName('edit-button');
+    editButtons.forEach(editBtn => {
+      editBtn.addEventListener('click', function(){
+        const attributeValue = this.getAttribute('data-id');
+        editId = editButton(attributeValue);
+         
+      });
+    });
+
+    // Update Button Todo List
+    updateBtn.addEventListener('click', ()=>{
+      updateTodoList(editId);
+    });
+
+ });
+
 
 
 
@@ -38,147 +70,121 @@
  })
 
 
-//  Add Todo List
- var addBtn = document.getElementById("add-btn");
- addBtn.addEventListener('click', () => {
-  if(todoVal !== "" && descriptionVal !== "" && dateVal !== ""){
-    id += 1;
-    // Store Todo to local storage
-    // arr_todo.push({'id' : id,'title': todoVal, 'description': descriptionVal, 'date': dateVal});
-    // localStorage.setItem('todoList', JSON.stringify(arr_todo));
-    // var getTodoList = localStorage.getItem('todoList');
-    // var arr_todoList = JSON.parse(getTodoList);
-    // Populate Todo List
-    // if(arr_todoList.length !== 0){
-    //   arr_todoList.forEach(item => {
-    //     var TodoList = document.getElementById("TodoList");
-     
-    //     card += `
-    //         <div class="card">
-    //         <div class="card-body">
-    //           <h3 class="">${item.title} ${item.id}</h3>
-    //           <p class="card-description">${item.description}</p>
-    //           <p class="card-date">${item.date}</p>
-    //         </div>
-    //         <div class="card-action">
-    //           <button  class="card-btn"><img class="card-btn-img" src="assets/pencil_drawing_480px.png" alt="pencil"></button>
-    //           <button data-id=${item.id} id="delete-btn${item.id}" class="card-btn"><img class="card-btn-img" src="assets/trash_480px.png" alt="trash"></button>
-    //         </div>
-    //       </div>
-    //       `;
 
-    //       TodoList.innerHTML = card;
-    //   })
-    // }
-    var newTodoList = store(arr_todo, id, todoVal, descriptionVal, dateVal);
-    retrieve(newTodoList, card);
-  
+ //  Retrieve Todo List
+ function retrieve(){
+  if(sessionStorage.getItem('todoList') != null){
+    card = "";
+    var todoListStorage = sessionStorage.getItem('todoList');
+    var toArray = JSON.parse(todoListStorage);
+    var TodoList = document.getElementById("TodoList");
+    toArray.map(item => {
+      card += `
+          <div class="card">
+          <div class="card-body">
+            <h3 class="">${item.title}</h3>
+            <p class="card-description">${item.description}</p>
+            <p class="card-date">${item.date}</p>
+          </div>
+          <div class="card-action">
+            <button data-id=${item.id} id="edit-btn${item.id}"   class="card-btn" name="edit-button"><img class="card-btn-img" src="assets/pencil_drawing_480px.png" alt="pencil"></button>
+            <button data-id=${item.id}  id="delete-btn" class="card-btn" name="delete-button"><img class="card-btn-img" src="assets/trash_480px.png" alt="trash"></button>
+          </div>
+        </div>
+        `;
+        TodoList.innerHTML = card;
+    });
   }
-
-  clear();
-
-  deleteTodoList();
-  // var currentTodoList = localStorage.getItem("todoList");
-  // var currentTodoListArr = JSON.parse(currentTodoList);
-  
-  // if(currentTodoListArr.length !== 0){
-  //   currentTodoListArr.forEach(item => {
-  //     var deleteBtn = document.getElementById(`delete-btn${item.id}`);
-  //     var data_id = deleteBtn.getAttribute('data-id');
-  //     var card = "";
-      
-  //     deleteBtn.addEventListener('click', () => {
-        
-  //       currentTodoListArr = currentTodoListArr.filter(todoArr => todoArr.id != data_id )
-  //       console.log(currentTodoListArr)
-        
-  //       var deleteTodoList = localStorage.setItem("todoList", JSON.stringify(currentTodoListArr));
-  //       var updatedTodoList = localStorage.getItem("todoList");
-  //       console.log(JSON.parse(updatedTodoList))
-  //       retrieve(JSON.parse(updatedTodoList), card);
-  //     });
-      
-  //   })
-  // }
-
- });
-
-
+}
 
 
 
 //  Store Todo List
-function store(arr_todo, id, todoVal, descriptionVal, dateVal ){
-  
-  arr_todo.push({'id' : id,'title': todoVal, 'description': descriptionVal, 'date': dateVal});
-  localStorage.setItem('todoList', JSON.stringify(arr_todo));
-  var getTodoList = localStorage.getItem('todoList');
-  var arr_todoList = JSON.parse(getTodoList);
-
-  return arr_todoList;
+function store( todoVal, descriptionVal, dateVal ){
+  // Check if there is current session storage
+  if(sessionStorage.getItem('todoList') != null){
+    var getTodoList = sessionStorage.getItem('todoList');
+    var toArray = JSON.parse(getTodoList);
+    var id = sessionStorage.getItem('id');
+    var data_id = 1 + parseInt(id);
+    toArray.push({'id' : data_id,'title': todoVal, 'description': descriptionVal, 'date': dateVal});
+    sessionStorage.setItem('todoList', JSON.stringify(toArray));
+    sessionStorage.setItem('id', data_id);
+    location.reload();
+    return toArray;
+  } else {
+    // push to local array
+    var data_id = 0;
+    var arr_todo = [];
+     arr_todo.push({'id' : data_id,'title': todoVal, 'description': descriptionVal, 'date': dateVal});
+    sessionStorage.setItem('todoList', JSON.stringify(arr_todo));
+    var id = sessionStorage.setItem('id', data_id);
+    location.reload();
+    return arr_todo;
+  }
 }
 
 
 
 // Delete Todo List
-function deleteTodoList(){
-  var currentTodoList = localStorage.getItem("todoList");
-  var currentTodoListArr = JSON.parse(currentTodoList);
-  
-
-  if(currentTodoListArr.length !== 0){
-    currentTodoListArr.forEach(item => {
-      var deleteBtn = document.getElementById(`delete-btn${item.id}`);
-      var data_id = deleteBtn.getAttribute('data-id');
-      
-      if(item.id == data_id){
-        deleteBtn.addEventListener('click', () => {
-        
-          currentTodoListArr = currentTodoListArr.filter(todoArr => todoArr.id != data_id )
-          console.log(currentTodoListArr)
-          
-          var updatedTodoList = localStorage.setItem("todoList", JSON.stringify(currentTodoListArr));
-          var updatedTodoList = localStorage.getItem("todoList");
-          var toArray = JSON.parse(updatedTodoList);
-          console.log(JSON.parse(updatedTodoList));
-          card = "";
-          arr_todo = toArray;
-          retrieve(toArray, card);
-        });
-      }
-      
-      
-    })
-  }
+function deleteTodoList(attributeId){
+    if(sessionStorage.getItem('todoList') != null){
+      var currentTodoList = sessionStorage.getItem("todoList");
+      var currentTodoListArr = JSON.parse(currentTodoList);
+      currentTodoListArr.map(item => {
+        if(attributeId == item.id){
+          currentTodoListArr = currentTodoListArr.filter(todoArr => todoArr.id != attributeId);
+          sessionStorage.setItem('todoList', JSON.stringify(currentTodoListArr));
+          location.reload();
+        }
+      });
+    }
 }
 
 
+// Edit button
+function editButton(attributeId){
+  if(sessionStorage.getItem('todoList') != null){
+    var currentTodoList = sessionStorage.getItem("todoList");
+    var currentTodoListArr = JSON.parse(currentTodoList);
+    var id;
 
-//  Retrieve Todo List
-  function retrieve(arr, card){
-    if(arr.length !== 0){
-      arr.forEach(item => {
-        var TodoList = document.getElementById("TodoList");
-     
-        card += `
-            <div class="card">
-            <div class="card-body">
-              <h3 class="">${item.title} ${item.id}</h3>
-              <p class="card-description">${item.description}</p>
-              <p class="card-date">${item.date}</p>
-            </div>
-            <div class="card-action">
-              <button  class="card-btn"><img class="card-btn-img" src="assets/pencil_drawing_480px.png" alt="pencil"></button>
-              <button data-id=${item.id}  id="delete-btn${item.id}" class="card-btn"><img class="card-btn-img" src="assets/trash_480px.png" alt="trash"></button>
-            </div>
-          </div>
-          `;
-
-          TodoList.innerHTML = card;
-      })
-    }
+    currentTodoListArr.map(item => {
+      if(attributeId == item.id){
+        updateBtn.disabled = false;
+        addBtn.disabled = true;
+        todo.value = item.title;
+        description.value = item.description;
+        date.value = item.date;
+        todoVal = item.title;
+        descriptionVal = item.description;
+        dateVal = item.date;
+        id = item.id
+      }
+    });  
+    return id;
   }
+}
 
+// Update button
+ function updateTodoList(attributeId){
+  var currentTodoList = sessionStorage.getItem('todoList');
+  var currentTodoListArr = JSON.parse(currentTodoList);
+  let updatedTodo = currentTodoListArr.map(item => {
+    if (item.id === attributeId) {
+
+        return { ...item, title: todoVal , description: descriptionVal, date: dateVal };  
+    }
+    return item;  
+  });
+  sessionStorage.setItem('todoList', JSON.stringify(updatedTodo));
+  location.reload();
+ }
+
+
+
+
+  // Clear  
   function clear(){
   todo.value = "";
   description.value = "";
